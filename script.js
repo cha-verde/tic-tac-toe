38b05dfa3cfae084a599829c62766d95e05d27cb
novi <novitrna@gmail.com>
2024-11-17 20:28:55 +0700

    Add interface and clean up logic

--- script.js
+++ script.js
@@ -3,70 +3,90 @@ const gameboard = (function () {
 
     let board = [];
 
-    for (let i = 0; i < 3; i++) {
-        let boardRow = [];
-        for (let j = 0; j < 3; j++) {
-            boardRow[j] = 'e';
+    function initializeBoard(){
+        for (let i = 0; i < 3; i++) {
+            let boardRow = [];
+            for (let j = 0; j < 3; j++) {
+                boardRow[j] = '';
+            }
+            board[i] = boardRow;
         }
-        board[i] = boardRow;
     }
 
-    console.log(board);
+    initializeBoard();
 
+    function refreshBoard(){
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                fillBox(i, j);
+            }
+        }
+    }
 
+    function fillBox(number1, number2) {
+        const box = document.querySelector('[data-row="' + number1 + '"][data-column="' + number2 + '"]');
+        box.textContent = board[number1][number2];
+    }
+
+    refreshBoard();
 
     function createPlayer(name, shape) {
-        return { shape };
+        return { name, shape };
     }
 
-    const playerOne = createPlayer("one", "X");
-    const playerTwo = createPlayer("two", "0");
+    const playerOne = createPlayer("Player 1", "X");
+    const playerTwo = createPlayer("Player 2", "0");
 
-    const players = [playerOne, playerTwo];
+    let currentBoxCount = 0;
 
     let currentPlayer = 0;
 
-    function updatePosition(row, column) {
-        if (currentPlayer == 0) {
-            if (board[row][column] != 'e') {
-                console.log("That position is filled, please choose another position!");
-            }
-            else {
-                board[row][column] = "X";
-                if (checkWinner(playerOne.shape)) {
-                    console.log("Player 1 Wins!");
-                }
-                else {
-                    if (checkGameOver()) {
-                        console.log("It's a tie!");
-                    }
-                    else {
-                        switchPlayer();
+    const displayBoard = document.querySelector('.board'); 
 
-                    }
-                }
-            }
-        }
-        else {
-            if (board[row][column] != 'e') {
-                console.log("That position is filled, please choose another position!");
-            }
-            else {
-                board[row][column] = "O";
-                if (checkWinner(playerTwo.shape)) {
-                    console.log("Player 2 Wins!")
-                }
-                else {
-                    if (checkGameOver()) {
-                        console.log("It's a tie!");
-                    }
-                    else {
-                        switchPlayer();
+    const resetButton = document.querySelector('.reset-button');
 
-                    }
-                }
-            }
+    resetButton.disabled = true;
+    
+    const statusText = document.querySelector('.status-text')
+
+    statusText.textContent = "Now it's Player 1 turn."
+
+    resetButton.addEventListener("click", () => {
+        board = [];
+        initializeBoard();
+        refreshBoard();
+        resetButton.disabled = true;
+        displayBoard.style.pointerEvents = "auto";
+        statusText.textContent = "Now it's Player 1 turn."
+        currentPlayer = 0;
+        currentBoxCount = 0;
+    })
+    function updatePosition(row, column, player) {
+        if (board[row][column] != '') {
+            console.log("That position is filled, please choose another position!");
+            return;
         }
+
+        board[row][column] = player.shape;
+        currentBoxCount++;
+        if (currentBoxCount == 9) {
+            refreshBoard();
+            statusText.textContent = "It's a tie!";
+            resetButton.disabled = false;
+            return;
+        }
+
+        if (checkWinner(player.shape)) {
+            refreshBoard();
+
+            statusText.textContent = player.name + " Wins!";
+            displayBoard.style.pointerEvents = "none";
+            resetButton.disabled = false;
+            return;
+        }
+        refreshBoard();
+        switchPlayer();
+
     }
 
 
@@ -74,31 +94,35 @@ const gameboard = (function () {
     function switchPlayer() {
         if (currentPlayer == 0) {
             currentPlayer = 1;
-            console.log("Now it's Player 2 turn");
+            statusText.textContent = "Now it's Player 2 turn";
         }
         else {
             currentPlayer = 0;
-            console.log("Now it's Player 1 turn");
+            statusText.textContent = "Now it's Player 1 turn";
         }
     }
 
     function checkWinner(player) {
         //horizontal
         for (let i = 0; i < 3; i++) {
-            if (board[i] != player) {
-                break;
-            }
-            if (i == 2) {
-                return true;
+            for (let j = 0; j < 3; j++) {
+                if (board[i][j] != player) {
+                    break;
+                }
+                if (j == 2) {
+                    return true;
+                }
             }
         }
         //vertical
         for (let i = 0; i < 3; i++) {
-            if (board[i][0] != player) {
-                break;
-            }
-            if (i == 2) {
-                return true;
+            for (let j = 0; j < 3; j++) {
+                if (board[j][i] != player) {
+                    break;
+                }
+                if (j == 2) {
+                    return true;
+                }
             }
         }
         //diagonal
@@ -121,24 +145,25 @@ const gameboard = (function () {
         }
     }
 
-    function checkGameOver() {
-        for (let k = 0; k < 3; k++) {
-            for (let l = 0; l < 3; l++) {
-                if (board[k][l] == 'e') {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
+
+    document.querySelectorAll('.shape-box').forEach(box => 
+        box.addEventListener('click', () => playGame(box.dataset.row, box.dataset.column))
+      );
+      
 
     const playGame = function (row, column) {
-        updatePosition(row, column)
+        if (currentPlayer == 0) {
+            updatePosition(row, column, playerOne);
+        }
+        else {
+            updatePosition(row, column, playerTwo);
+        }
         return board;
     }
 
 
     return { playGame };
 })();
+
 
 
